51a7f54cdbf8406ac94c416095a291955cfdb305
Spimiki <81101227+Spimiki@users.noreply.github.com>
2022-12-22 19:46:24 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,12 +23,12 @@ function loading() {
             document.body.style.cursor = 'none'
             document.body.style.overflow = 'hidden'
             document.body.innerHTML = ` `;
-        }, number * x + 100);
+        }, number * x + 1000);
     }
 
 };
 
-window.addEventListener("load", setTimeout(() => {
+mid.addEventListener("load", setTimeout(() => {
     number = prompt('Enter number between 100 and 100000000.\nHigher number = longer update screen', 10000)
     if (number < 100) {
         number = 100
