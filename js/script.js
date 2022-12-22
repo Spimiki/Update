68c4afa240dab3fff5a3600280df46179b4d9888
Spimiki <81101227+Spimiki@users.noreply.github.com>
2022-12-22 19:25:49 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,7 +5,9 @@ let percent = 0;
 
 midtxt.innerHTML = `Working on update ${percent}% complete. <br> Don't turn off your PC. This will take a while.`
 
-function loading(number) {
+let number = 10000
+
+function loading() {
     let x = Math.random();
     let time = x * number;
     if (percent != 100) {
@@ -27,12 +29,12 @@ function loading(number) {
 };
 
 window.addEventListener("load", setTimeout(() => {
-    let number = prompt('Enter number between 100 and 100000000.\nHigher number = longer update screen', 10000)
+    number = prompt('Enter number between 100 and 100000000.\nHigher number = longer update screen', 10000)
     if (number < 100) {
         number = 100
     }
     if (number > 100000000) {
         number = 100000000
     }
-    loading(number)
+    loading()
 }, 1000));
